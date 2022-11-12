8dc82f4398d49c36f766a117595ee56b29777798
akbar615317 <akbarmca09@gmail.com>
2022-11-12 22:31:52 +0530

    "second commit"

--- rsk/src/app/test/test.component.ts
+++ rsk/src/app/test/test.component.ts
@@ -4,10 +4,14 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-test',
   template: `
   <h1>  {{name}} </h1>
+<button (click)="onClick()" type="button">
+Click me
+
+</button>
+<h1> {{greetings}} </h1>
+
     <p>
       test works!
-
-      
     </p>
   `,
   styles: [`
@@ -27,8 +31,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
-  public name = "Test";
+  public name = "Test12";
+
+  public greetings="";
   constructor() { }
+  onClick()
+  {
+    this.greetings="welcome to 2 way binding"
+    //console.log("two way binding");
+  }
 
   ngOnInit(): void {
   }
